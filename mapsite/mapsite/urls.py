436a5c764af6d7bf001d3020af7179c1ad5ca6c5
Ask Iver <sylteaskiver@yahoo.com>
2023-09-26 03:32:12 +0200

    TKP-34 Add logic for user registration

--- mapsite/mapsite/urls.py
+++ mapsite/mapsite/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import SignUpView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- mapsite/mapsite/views.py
+++ mapsite/mapsite/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
 
 
 def index(request):
     return HttpResponse("Current logged in user is " + request.user.username + ".")
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
