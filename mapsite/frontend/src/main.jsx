88d5daa7773eefc30bd6d23184e600deaf926ffc
lukavucenovic <lukavu1999@gmail.com>
2023-10-01 16:08:26 +0200

    TKP-27 Adding router to frontend

--- mapsite/frontend/src/main.jsx
+++ mapsite/frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+
 import App from './App.jsx'
+import Map from './routes/Map.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <App/>
+  },
+  {
+      path: "/map",
+      element: <Map/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ mapsite/frontend/src/routes/Map.jsx
@@ -0,0 +1,10 @@
+
+export default function Map() {
+    return(
+        <>
+            <div>
+                <h1>This is the map page</h1>
+            </div>
+        </>
+    )
+}
