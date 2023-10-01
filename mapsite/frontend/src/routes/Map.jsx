60687c7f1c10891f219a0755c156f131337a9e79
lukavucenovic <lukavu1999@gmail.com>
2023-10-01 17:38:42 +0200

    TKP-27 Adding different map type

--- mapsite/frontend/src/routes/Map.jsx
+++ mapsite/frontend/src/routes/Map.jsx
@@ -6,7 +6,7 @@ export default function Map() {
           <MapContainer center={[63.430479, 10.394970]} zoom={13} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
             />
             <Marker position={[63.430479, 10.394970]}>
                 <Popup>
