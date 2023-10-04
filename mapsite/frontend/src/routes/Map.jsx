cbc0af12b69112baed3ff36a340dde532c43e3d0
Dorthea Munch-Ellingsen Selnes <dmselnes@stud.ntnu.no>
2023-10-04 11:41:40 +0200

    Add search functionality

--- mapsite/frontend/src/routes/Map.jsx
+++ mapsite/frontend/src/routes/Map.jsx
@@ -1,19 +1,35 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useRef, useEffect } from 'react';
+import { MapContainer, TileLayer} from 'react-leaflet';
+import * as GeoSearch from 'leaflet-geosearch';
 
 export default function Map() {
-    return(
-        <>
-          <MapContainer center={[63.430479, 10.394970]} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            />
-            <Marker position={[63.430479, 10.394970]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-            </MapContainer>
-        </>
-    )
+
+// Create a ref for the map container
+const mapRef = useRef(null);
+
+useEffect(() => {
+  // Initialize the map
+  const map = mapRef.current;
+
+  if (map) {
+    // Create and add the GeoSearch control
+    const searchControl = new GeoSearch.GeoSearchControl({
+      provider: new GeoSearch.OpenStreetMapProvider(),
+      style: 'bar', // Customize the style here (bar or button)
+      searchLabel: 'Skriv inn adresse',
+    });
+    map.addControl(searchControl);
+  }
+});
+
+return (
+  <>
+    <MapContainer ref={mapRef} center={[63.430479, 10.394970]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+    </MapContainer>
+  </>
+);
 }
