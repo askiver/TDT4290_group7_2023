10b784bb6786b885da2db0f7c259bfbc8360c163
Linn Marie <lmpeders@stud.ntnu.no>
2023-11-13 10:43:39 +0100

    Revert "TKP-60: Changed the title"
    This reverts commit fc85ab4754e5120b8292077ea2a2d8ca5889ec2b.

--- mapsite/frontend/src/components/Navbar.jsx
+++ mapsite/frontend/src/components/Navbar.jsx
@@ -93,7 +93,7 @@ function Navbar() {
               color: "black",
             }}
           >
-            MATERIALBEHOLGNING I BYGG
+            TITTEL
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
